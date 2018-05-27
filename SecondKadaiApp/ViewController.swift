5f1c8f4bdc28637f3b7d6a9f2fc09d42e74cc00e
hidetoshi.kanno <taf71811@docomo.ne.jp>
2018-05-27 12:59:28 +0900

    Time:	2018/5/27 Who:	HidetoshiKanno Version:	1.0

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //変数定義
     @IBOutlet weak var InputNameValue_1: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +24,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // 遷移直前準備
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //　遷移先クラス定義
         let showNameViewController : ShowNameViewController = segue.destination as! ShowNameViewController
         
+        //遷移先への値引き渡し
         if( InputNameValue_1.text != ""){
             showNameViewController.Name_1 = InputNameValue_1.text!
+            showNameViewController.Flag = true
         } else { print("NoInput") }
     }
     
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        // 他の画面から segue を使って戻ってきた時に呼ばれる
+        print("Return")
+    }
+
 }
+
+
+
+
+
 
