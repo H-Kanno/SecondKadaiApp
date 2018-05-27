b9b260fb4a348004f1a9c15be6d37eaf4a57c55e
hidetoshi.kanno <taf71811@docomo.ne.jp>
2018-05-27 12:31:30 +0900

    Time: 2018/5/27 Who: HidetoshiKanno Version: first version

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var InputNameValue_1: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let showNameViewController : ShowNameViewController = segue.destination as! ShowNameViewController
+        
+        if( InputNameValue_1.text != ""){
+            showNameViewController.Name_1 = InputNameValue_1.text!
+        } else { print("NoInput") }
+    }
+    
 }
 
